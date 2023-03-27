804c7cd53b06efccb5d73c8f7dae5e568a4dc88c
Andrii_Tutyk <andreww@ukr.net>
2023-03-27 19:12:47 +0300

    initial commit

---
+++ awc-ec2-homework/src/main/java/com/example/awcec2homework/AwcEc2HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.awcec2homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwcEc2HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwcEc2HomeworkApplication.class, args);
+    }
+
+}
